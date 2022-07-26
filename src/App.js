6376d8f849810d67b2eefef30c2bb2c74e5f0034
Elder <elderfavsto@gmail.com>
2022-07-26 14:13:23 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+        heigth="100vh"
+        projectID="555646ff-8879-4f2b-b1af-13c50565e7c0"
+        userName='José'
+        userSecret='jesus'
+        renderChatFeed= {(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage = {() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      />
     </div>
   );
 }
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,10 @@
+const MyMessage = ({message}) => {
+  if (message.attachments && )
+  return (
+    <div>
+      mY message
+    </div>
+  )
+}
+
+export default MyMessage;
